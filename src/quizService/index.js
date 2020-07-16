b79b5ecf5c09fabe141d2a67f78068306a96370f
RandwipGh <rg700205@gmail.com>
2020-07-16 21:24:34 +0530

    initial commit

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "How have you been?  ",
+    answers: ["Breathing aru", "ekdom jui", "jen ten jiyai", "dead"],
+    correct: "jen ten jiyai",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Where is the train station "Llanfair­pwllgwyngyll­gogery­chwyrn­drobwll­llan­tysilio­gogo­goch"?',
+    answers: ["Wales", "Moldova", "Czech Republic", "Denmark"],
+    correct: "Wales",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Source of Orihime's powers",
+    answers: ["Hairpins", "chi", "magic lamp", "DD"],
+    correct: "Hairpins",
+    questionId: "267908"
+  },
+  {
+    question: "What's the name of Batman's  parents?",
+    answers: [
+      "Thomas & Martha",
+      "Joey & Jackie",
+      "Jason & Sarah",
+      "Todd & Mira"
+    ],
+    correct: "Thomas & Martha",
+    questionId: "333247"
+  },
+  {
+    question: "Who is the strongest Espada",
+    answers: ["Menoly", "Coyote Stark", "Grimmjow", "Ulquiora"],
+    correct: "Coyote Stark",
+    questionId: "496293"
+  },
+  {
+    question:
+      "Who helped ichigo with his speed",
+    answers: [
+      "Yoruichi the black cat",
+      "Rukia",
+      "chad",
+      "Kenpachi zaraki"
+    ],
+    correct: "Yoruichi the black cat",
+    questionId: "588909"
+  },
+  {
+    question:
+      "What is ishida's hidden talent",
+    answers: ["Sews like a boss", "Can eat a lot", "good at sports", "can sing"],
+    correct: "Sews like a boss",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "how much harrassement did your colege give you this lockdown",
+    answers: ["none", "okn", "AEC hoi so kolpona t ke besi", "100/100"],
+    correct: "AEC hoi so kolpona t ke besi",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Strongest among the 7 sins",
+    answers: [
+      "Lion sin Escanor",
+      "Dragon sin Meliodas Sama",
+      "Boar sin Merlin",
+      "fox sin Ban"
+    ],
+    correct: "Lion sin Escanor",
+    questionId: "98390"
+  },
+  {
+    question: "Who is the non living sin",
+    answers: ["Diane", "Ban", "king", "Gowther"],
+    correct: "Gowther",
+    questionId: "1071006"
+  },
+  {
+    question: "Best avatar of all time",
+    answers: ["Roku", "Kyoshi", "Aang", "Korra"],
+    correct: "Korra",
+    questionId: "1174154"
+  },
+  {
+    question: "Oldest living creature in Avatar's world is",
+    answers: ["Lion Turtle", "bison", "dragon", "Momo"],
+    correct: "Lion Turtle",
+    questionId: "1226535"
+  },
+  {
+    question: "Most used assamese word",
+    answers: ["hoiniki", " kene", "Dhekeri", "Kela"],
+    correct: "Kela",
+    questionId: "1310938"
+  },
+  {
+    question: "Saitama is afan of which other anime character?",
+    answers: ["Luffy", "Goku", "Itachi", "Mob"],
+    correct: "Mob",
+    questionId: "1436873"
+  },
+  {
+    question: "Who finally beats cell",
+    answers: ["Gotan", "Gohan", "Vegeta", "Goku"],
+    correct: "Gohan",
+    questionId: "1515110"
+  },
+  {
+    question: "On a dartboard, what number is directly opposite No. 1?",
+    answers: ["19", "20", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "Who becomes the no.1 hero after All might ",
+    answers: ["Best jeanist", "Endeavor", "Hawks", "Edgeshot"],
+    correct: "Endeavor",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "One Punch Man Saitama's look was inspired from which character",
+    answers: ["Aang", "Krillin", "Jhonny Sins", "Anpanman"],
+    correct: "Anpanman",
+    questionId: "1822532"
+  },
+  {
+    question: "Who is the most annoying character in DBz history",
+    answers: [
+      "Freiza",
+      "Majin buu",
+      "Mr. Satan",
+      "Babidi"
+    ],
+    correct: "Mr. Satan",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "what powers does Mob have?",
+    answers: ["Elemental", "Mental", "Physical", "Psychic"],
+    correct: "Psychic",
+    questionId: "2134343"
+  },
+  {
+    question: "Which one of the following rhythm games was made by Harmonix?",
+    answers: [
+      "Rock Band",
+      "Meat Beat Mania",
+      "Guitar Hero Live",
+      "Dance Dance Revolution"
+    ],
+    correct: "Rock Band",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "What type of animal was Harambe, who was shot after a child fell into it's enclosure at the Cincinnati Zoo?",
+    answers: ["Gorilla", "Tiger", "Panda", "Crocodile"],
+    correct: "Gorilla",
+    questionId: "2379831"
+  },
+  {
+    question: "What is L's surname",
+    answers: ["Lawliet", "yagami", "ohba"],
+    correct: "Lawliet",
+    questionId: "2426418"
+  },
+  {
+    question: "Which DeathNote fan theory was declared as canon later by writer",
+    answers: ["None", "Light would die in end", "There was a DeathNote eraser", "L knows capoeira"],
+    correct: "L knows capoeira",
+    questionId: "2510086"
+  },
+  {
+    question: "Other than shikamaru who was promoted to Jounin after the great war",
+    answers: [
+      "Hinata",
+      "Sakura",
+      "sasuke",
+      "inojin"
+    ],
+    correct: "Sakura",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Youngest member of the akatsuki",
+    answers: ["Deidara", "kakuzu", "Itachi", "sasori"],
+    correct: "Deidara",
+    questionId: "2796884"
+  },
+  {
+    question: "Why is naruto s fake arm under bandages",
+    answers: ["It would turn to tree while in kurama mode", "belong to softy kimetsu no yaiba era", "donno", "jff"],
+    correct: "it would turn into tree while in kurama mode",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the French word for "hat"?',
+    answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Total number of bridges over the river Brahmaputra",
+    answers: ["1", "3", "5", "6"],
+    correct: "6",
+    questionId: "3096579"
+  },
+  {
+    question: "First rapper to win a grammy",
+    answers: [
+      "Eminem",
+      "Will Smith",
+      "Royce 59",
+      "Crazy Kanye"
+    ],
+    correct: "Will Smith",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Which is the Assam state Animal?",
+    answers: ["Homonculus", "Tiger", "wild boar", "one horned rhino"],
+    correct: "one horned rhino",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
+    answers: ["Truth", "Answer", "Cause", "Source"],
+    correct: "Truth",
+    questionId: "3410327"
+  },
+  {
+    question: "What is an official language of Assam state?",
+    answers: ["Assamese", "Bengali", "Hindi", "English"],
+    correct: "Assamese",
+    questionId: "3542596"
+  },
+  {
+    question: "Tastiest meat on this planet",
+    answers: ["Seriously?", "patha", "siken", "Pork"],
+    correct: "Pork",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: " Which of the following fauna is found only in the Assam state of India?",
+     answers: [" Pygmy hog", "Clouded leopard", "Gee’s golden langur", "Sloth bear"],
+    correct: "Pygmy hog",
+    questionId: "3893585"
+  },
+  {
+    question: " Which is the Assam state Bird?",
+    answers: [ "White winged duck", "Asian koelC", "Western TragopanD", "Black francolin"],
+    correct: "White winged duck",
+    questionId: "3913430"
+  },
+  {
+    question: "With context to a culture of Assam, what is ‘Jaapi’?",
+    answers: ["knitted fabric", "tribe of assam", "najanu", "Conical hat made from bamboo"],
+    correct: "conical hat made from bamboo",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following card games revolves around numbers and basic math?",
+    answers: ["Uno", "Go Fish", "Twister", "Munchkin"],
+    correct: "Uno",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What machine element is located in the center of fidget spinners?",
+    answers: ["Bearings", "Axles", "Gears", "Belts"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Lockdown ot ki korila apa/api?",
+    answers: ["Lilimai on phn", "pohilu(mokkelz)", "AEC student janai aru :(", "only khua n hua"],
+    correct: "AEC student janai aru :(",
+    questionId: "4321002"
+  },
+  {
+    question: " Which is the largest coffee producing state of India?",
+    answers: [
+      "Karnataka",
+      "Sapekhati(jk its smoltown)",
+      "Mumbai",
+      "Tezpur(ami saa khau o)"
+    ],
+    correct: "Karnataka",
+    questionId: "4442286"
+  },
+  {
+    question: "What is the state flower of Haryana?",
+    answers: [
+      "Lotus",
+      "Weed flower",
+      "Rose",
+      "surajmukhi"
+    ],
+    correct: "Lotus",
+    questionId: "4564430"
+  },
+  {
+    question: "What company developed the vocaloid Hatsune Miku?",
+    answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
+    correct: "Crypton Future Media",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which country, not including Japan, has the most people of japanese decent?",
+    answers: ["Brazil", "China", "South Korea", "United States of America"],
+    correct: "Brazil",
+    questionId: "4795960"
+  },
+  {
+    question: "Which of the following is the capital of Arunachal Pradesh?",
+    answers: ["Itanagar","Imphal","Tezpur","Sapekhati"],
+    correct: "Itanagar",
+    questionId: "4811162"
+  },
+  {
+    question: "India is a federal union comprising twenty-nine states and how many union territories?",
+    answers: ["6","7","8","9"],
+    correct: "7",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
